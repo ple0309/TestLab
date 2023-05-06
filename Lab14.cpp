a36b46a92ba83640e865848994c15939e535b81b
Phuc Le <ple030999@gmail.com>
2023-05-05 21:44:28 -0700

    First Commit

---
+++ Lab14.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>   
+using namespace std;
+
+int main()
+{
+    string firstName;
+
+    cout <<"Enter your first name: " << endl;
+    cin >> firstName;
+
+    cout << "Hello " << firstName << endl;
+    
+    return 0;
+}
